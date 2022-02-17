a38509377a4c11c9a32d12ec7d837742bcb71dfc
ferain <ferain@kakao.com>
2022-02-18 01:42:41 +0900

    JS Master

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-import _ from "lodash"
-import getType from "./getType"
-import random from "./getRandom"
+import axios from 'axios'
+
+fetchMovies('frozen')
+
+//영화 찾기 함수
+function fetchMovies(movie) {
+  axios
+    .get('https://omdbapi.com/?apikey=7035c60c&s=' + movie)
+    .then((res) => {
+      console.log(res)
+      const pEl = document.querySelector('p')
+      const imgEl = document.querySelector('img')
+      pEl.textContent = res.data.Search[0].Title
+      imgEl.src = res.data.Search[0].Poster
+    })
+}
